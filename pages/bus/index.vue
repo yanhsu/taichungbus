529ccd505ce975a01345e2486717cd4e2e579fd4
yanhsu <e31217@gmail.com>
2018-04-29 12:01:55 +0800

    add search

--- pages/bus/index.vue
+++ pages/bus/index.vue
@@ -1,10 +1,17 @@
 <template>
 		<div class="bus">
-				<b-col cols='12' v-for="(route, index) in busRoute" :key="index">
+				<b-navbar type="dark" variant="info">
+					<b-navbar-nav :justified=true>
+						<b-nav-form>
+							<b-form-input  class="mr-sm-2" type="text" v-model="search" placeholder="搜尋公車路線"/>
+						</b-nav-form>
+					</b-navbar-nav>
+				</b-navbar>
+				<b-col cols='12' class="card-item" v-for="(route, index) in busRoute" :key="index" v-show="(search == null || search == '') || (route.RouteName.indexOf(search) >= 0) && route.RouteName.indexOf(search[0]) == 0">
 						<b-row>
 								<b-col cols='3'></b-col>
 								<b-col cols='6' class="px-0">
-										<b-list-group-item variant="success" class="mt-3 d-flex align-items-center list-item">
+										<b-list-group-item variant="success"  class="mt-3 d-flex align-items-center list-item">
 												<b-col cols=9>
 														<label class="mr-2 my-0"> {{route.RouteName}} </label> 
 														<label class="mr-2 my-0">{{ route.Departure }}</label> 
@@ -34,9 +41,21 @@ export default {
 						return this.$store.state.bus.list
 				}
 		},
+		data () {
+			return {
+				search: null,
+			}
+		},
 		methods: {
 			getOneBusInfo: function (id,q) {
 				this.$router.push(`/bus/${id}?q=${q}`)
+			},
+			searchBus: function (search, id) {
+				if(search == null || search == '') {
+					return true
+				} else if (id.indexOf(search) > 0) {
+					return true
+				} else return false
 			}
 		}
 }
